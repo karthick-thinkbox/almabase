05fa7cd84d1d17f386e90a19b9c4e575462fe119
karthick-thinkbox <karthick.thinkbox@gmail.com>
2017-02-10 16:05:54 +0530

    Update wsgi.py

--- Almabase/wsgi.py
+++ Almabase/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for Almabase project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path='/home/webspacebykarthick/almabase'
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] ='Almabase.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Almabase.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application=StaticFilesHandler(get_wsgi_application())
