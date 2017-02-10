2deff6afe69144c3bef0f5fabb0e55dd7ca41ebb
karthick-thinkbox <karthick.thinkbox@gmail.com>
2017-02-10 16:07:40 +0530

    Update models.py

--- Game/models.py
+++ Game/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Inventory(models.Model):
-    title = models.CharField(max_length=56, blank=True,primary_key=True)
+    title = models.CharField(max_length=56, blank=True)
     platform = models.CharField(max_length=16, blank=True, null=True)
     score = models.DecimalField(max_digits=2, decimal_places=1, blank=True, null=True)
     genre = models.CharField(max_length=17, blank=True, null=True)
